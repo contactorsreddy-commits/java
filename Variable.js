687d46ba0bf4ba4fe5043acaf546efee044f190f
naithik <contact.orsreddy@gmail.com>
2026-07-03 10:45:48 -0400

    variable

---
+++ Variable.js
@@ -0,0 +1,9 @@
+const my_name="naithik";
+let my_age=9;
+const hobby=["sports", "video games"];
+console.log("personal information");
+console.log("my name is "+my_name);
+console.log("my age is"+my_age);
+console.log("my hobbies are"+hobby);
+const my_school="Mary. C. House";
+console.log("my school name is "+my_school);
