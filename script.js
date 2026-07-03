3b1470a298b9fb47be9b02b7bc3114d441437d58
naithik <contact.orsreddy@gmail.com>
2026-07-03 10:14:50 -0400

    initiai

---
+++ script.js
@@ -0,0 +1,10 @@
+console.log("hello how are you");
+console.log("a");
+console.log(2);
+console.log(20 + 30);
+console.log(40 - 30);
+console.log(30 * 60);
+console.log(70 / 50);
+console.warn("this a warning message");
+console.error("this is a error message");
+
