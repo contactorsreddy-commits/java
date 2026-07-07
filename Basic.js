f516f6d6c88911402cbb7b0a87f10941e37570f6
naithik <contact.orsreddy@gmail.com>
2026-07-07 10:40:22 -0400

    condition

---
+++ Basic.js
@@ -0,0 +1,14 @@
+var favorite_color=prompt("what is your favorite color:");
+if(favorite_color=="blue"){
+    console.log("your favorite color is the color of the sky");
+
+}
+else if(favorite_color=="red"){
+    console.log("your favorite color is the color of roses");
+}
+else if(favorite_color=="green"){
+    console.log("your favorite color is the color of grass");
+}
+else{
+    console.log("wow ,"+favorite_color+" is awesome color");
+}
