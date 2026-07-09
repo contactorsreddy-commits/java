79f12c8afc65788e5d72d8c491e540213438d4b2
naithik <contact.orsreddy@gmail.com>
2026-07-09 10:19:24 -0400

    function opiraters

---
+++ parameter.js
@@ -0,0 +1,20 @@
+function functionaddition(x,y)
+{
+    return x+y;
+}
+function functionsubtraction(a,b)
+{
+    return a-b;
+}
+function functionmultiplication(c,d)
+{
+    return c*d;
+}
+function functiondivision(e,f)
+{
+    return e/f;
+}
+console.log(functionaddition(20,30));
+console.log(functionsubtraction(40,50));
+console.log(functionmultiplication(60,70));
+console.log(functiondivision(90,80));
