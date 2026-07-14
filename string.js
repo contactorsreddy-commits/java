067695cdccb3003c647c312d0c36d95d849d18f6
naithik <contact.orsreddy@gmail.com>
2026-07-14 10:37:26 -0400

    stringfunction

---
+++ string.js
@@ -0,0 +1,8 @@
+let text="welcome to the text transformer";
+console.log("Uppertext"+text.toUpperCase());
+console.log("Lowertext"+text.toLowerCase());
+console.log("find the character "+text.charAt(0));
+console.log("substring "+text.substring(2));
+console.log("substring "+text.substring(2,8));
+console.log("index of"+text.indexOf("e"));
+console.log("index of"+text.indexOf("f"));
