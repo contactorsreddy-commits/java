2b17876d0713532fd213cf17f37aaafb5dc9fec4
naithik <contact.orsreddy@gmail.com>
2026-07-15 11:05:53 -0400

    stringwizard

---
+++ stringwizard.js
@@ -0,0 +1,6 @@
+var inputtext = prompt("Hello world");
+var uppercasetext = inputtext.toUpperCase();
+var lowercasetext = inputtext.toLowerCase();
+
+console.log("Uppercase: " + uppercasetext);
+console.log("Lowercase: " + lowercasetext);
