57b7008776c2ff6c1eacbb1d73ea8185eb9449e2
naithik <contact.orsreddy@gmail.com>
2026-07-16 10:20:57 -0400

    fruitpicker

---
+++ fruitpicker.js
@@ -0,0 +1,4 @@
+let fruits = ["apple", "banana", "pineapple", "orange", "grape", "kiwi", "mango", "cherries", "peach", "watermelon",];
+let randomindex = Math.floor(Math.random() * 10);
+let randomfruit = fruits[randomindex];
+console.log("Random fruit of the day", randomfruit);
