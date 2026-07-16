d7479ed3e01a06838af4d9212969ec93b6fed2ba
naithik <contact.orsreddy@gmail.com>
2026-07-16 11:04:44 -0400

    sports

---
+++ stport.js
@@ -0,0 +1,4 @@
+let favoriteSports = ["Cricket", "American football", "football", "tennis", "badminton", "basketball", "volleyball"];
+console.log("My favorite sports are: = ", favoriteSports);
+let newSport = prompt("Enter your favorite sport");
+console.log("New favorite sport is:", favoriteSports + "," + newSport);
